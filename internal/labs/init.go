439750300ff4ced5bdc2eddfc2296ed99194c25a
faxryzen <cheburek2006@gmail.com>
2026-06-30 04:32:00 +0300

    last

--- internal/labs/init.go
+++ internal/labs/init.go
@@ -25,11 +25,26 @@ func FetchLabsToJSON(repo f.Repository) ([]byte, error) {
 	mergedPRs := ConvertToLabs(prs["merged"])
 	openPRs   := ConvertToLabs(prs["open"])
 
+	mergedPRsFixed := []f.PullRequest{}
+	openPRsFixed :=   []f.PullRequest{}
+
 	for i := range mergedPRs {
-		CalculateScore(&mergedPRs[i], labs[mergedPRs[i].Name])
+		lab_cfg, exist := labs[openPRs[i].Name]
+		if exist {
+			CalculateScore(&mergedPRs[i], lab_cfg)
+			mergedPRsFixed = append(mergedPRsFixed, mergedPRs[i])
+		}
 	}
 
-	j, err := json.MarshalIndent(append(mergedPRs, openPRs...), "", " ")
+	for i := range openPRs {
+		lab_cfg, exist := labs[openPRs[i].Name]
+		if exist {
+			CalculateScore(&openPRs[i], lab_cfg)
+			openPRsFixed = append(openPRsFixed, openPRs[i])
+		}
+	}
+
+	j, err := json.MarshalIndent(append(mergedPRsFixed, openPRsFixed...), "", " ")
 	if err != nil {
 		return nil, err
 	}
